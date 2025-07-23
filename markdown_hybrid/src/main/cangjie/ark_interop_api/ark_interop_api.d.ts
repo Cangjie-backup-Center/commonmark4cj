1584aa33bf173148fa2a0ba000a6d6caf17f861a
yangtao <taoyangdd@isoftstone.com>
2025-07-23 16:26:20 +0800

    5.1.1.821

---
+++ markdown_hybrid/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
@@ -0,0 +1,34 @@
+export declare class CjNode {
+    getNext(): CjNode | undefined
+    getPrevious(): CjNode | undefined
+    getFirstChild(): CjNode | undefined
+    getLastChild(): CjNode | undefined
+    getParent(): CjNode | undefined
+    appendChild(child: CjNode): void
+    prependChild(child: CjNode): void
+    unlink(): void
+    insertAfter(sibling: CjNode): void
+    insertBefore(sibling: CjNode): void
+    toString(): string
+    getNodeType(): string
+    getLiteral(): string | undefined
+    getDestination(): string | undefined
+    getTitle(): string | undefined
+    getLabel(): string | undefined
+    getOpeningDelimiter(): string | undefined
+    getClosingDelimiter(): string | undefined
+    getFenceChar(): string | undefined
+    getFenceLength(): number | undefined
+    getFenceIndent(): number | undefined
+    getInfo(): string | undefined
+    isTight(): boolean | undefined
+    getBulletMarker(): string | undefined
+    getStartNumber(): number | undefined
+    getDelimiter(): string | undefined
+}
+
+export declare interface CustomLib {
+    CjNode: {new (): CjNode}
+    printNode(node: CjNode | undefined): void
+    parseIntoCjNode(md: string): Promise<CjNode>
+}
---
+++ markdown_hybrid/src/main/cangjie/types/libmarkdown_hybrid/Index.d.ts
@@ -0,0 +1,36 @@
+export declare class CjNode {
+    getNext(): CjNode | undefined
+    getPrevious(): CjNode | undefined
+    getFirstChild(): CjNode | undefined
+    getLastChild(): CjNode | undefined
+    getParent(): CjNode | undefined
+    appendChild(child: CjNode): void
+    prependChild(child: CjNode): void
+    unlink(): void
+    insertAfter(sibling: CjNode): void
+    insertBefore(sibling: CjNode): void
+    toString(): string
+    getNodeType(): string
+    getLiteral(): string | undefined
+    getDestination(): string | undefined
+    getTitle(): string | undefined
+    getLabel(): string | undefined
+    getOpeningDelimiter(): string | undefined
+    getClosingDelimiter(): string | undefined
+    getFenceChar(): string | undefined
+    getFenceLength(): number | undefined
+    getFenceIndent(): number | undefined
+    getInfo(): string | undefined
+    isTight(): boolean | undefined
+    getBulletMarker(): string | undefined
+    getStartNumber(): number | undefined
+    getDelimiter(): string | undefined
+    constructor ()
+}
+
+export declare function testCJ(s: string): string
+export declare function printNode(node: CjNode | undefined): void
+
+export declare function parseIntoCjNode(md: string): Promise<CjNode>
+
+export declare function parseIntoJsNode(md: string, JsNode: () => object, ...customParsers: Array<object>): object
